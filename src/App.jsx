02590286ae66ec1daab0e3fd5865bd13a5716a46
Dincz <dineshndr02@gmail.com>
2024-01-17 14:03:05 +0530

    Added Login Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import { Route, Routes } from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs.jsx';
 import HomePage from './Pages/HomePage.jsx';
+import Login from './Pages/Login.jsx';
 import NotFound from './Pages/NotFound.jsx';
 import Signup from './Pages/Signup.jsx';
+
 function App() {
 
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/" element={<HomePage />} ></Route>
         <Route path="/about" element={<AboutUs />} ></Route>
         <Route path="/signup" element={<Signup />} ></Route>
+        <Route path="/login" element={<Login />} ></Route>
+
         <Route path="*" element={<NotFound />} ></Route>
 
       </Routes>
