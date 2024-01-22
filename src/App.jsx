cce935503479ddb181f2eb057bb653bd2407e9c5
Dincz <dineshndr02@gmail.com>
2024-01-22 21:08:20 +0530

    Create course

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import './App.css';
 
 import { Route, Routes } from 'react-router-dom';
 
+import RequireAuth from './Components/Auth/requireAuth.jsx'
 import AboutUs from './Pages/AboutUs.jsx';
 import Contact from './Pages/ContactUs.jsx';
+import CourseDescription from './Pages/Course/CourseDescription';
 import CourseList from './Pages/Course/CourseList';
+import CreateCourse from './Pages/Course/CreateCourse';
+import Denied from './Pages/Denied.jsx';
 import HomePage from './Pages/HomePage.jsx';
 import Login from './Pages/Login.jsx';
 import NotFound from './Pages/NotFound.jsx';
@@ -19,7 +23,12 @@ function App() {
         <Route path="/signup" element={<Signup />} ></Route>
         <Route path="/login" element={<Login />} ></Route>
         <Route path="/courses" element={<CourseList />} />
+        <Route path="/course/description" element={<CourseDescription />} />
         <Route path="/Contact" element={<Contact />} />
+        <Route path="/Denied" element={<Denied />} />
+        <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
+          <Route path="/course/create" element={<CreateCourse />} />
+        </Route>
         <Route path="*" element={<NotFound />} ></Route>
 
       </Routes>
--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -69,7 +69,11 @@ function HomeLayout({ children }) {
                                 <Link to="/admin/dashboard"> Admin DashBoard</Link>
                             </li>
                         )}
-
+                        {isLoggedIn && role === 'ADMIN' && (
+                            <li>
+                                <Link to="/course/create"> Create new course</Link>
+                            </li>
+                        )}
                       {isLoggedIn && (
                         <li>
                             <Link to="/courses">All Courses</Link>
